1e03e388352a95e3651666645f35466effe6567a
artlenk.ru@gmail.com <df5ukoi7ht>
2017-02-18 18:54:26 +0300

    Create blog-inner pages

--- src/js/common.js
+++ src/js/common.js
@@ -31,3 +31,12 @@ function checkAndRepaint() {
 
     });
 })(jQuery);
+
+$('.related-posts-carousel').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 5,
+    nextArrow: '<span class="slick-next slick-arrow icon-next"></span>',
+    prevArrow: '<span class="slick-prev slick-arrow icon-prev"></span>'
+});
--- src/js/lib.js
+++ src/js/lib.js
@@ -2,4 +2,6 @@
 
 //= lib/jquery.formstyler.min.js
 
+//= ../../bower_components/slick-carousel/slick/slick.min.js
+
 // = lib/jquery.fancybox.min.js
