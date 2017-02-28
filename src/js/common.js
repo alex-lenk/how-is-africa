bf1c5142118d7d41253c6935ae9d92adca41bdcc
artlenk.ru@gmail.com <df5ukoi7ht>
2017-02-28 21:29:19 +0300

    Fixed minor bug in blog

--- src/js/common.js
+++ src/js/common.js
@@ -1,30 +1,42 @@
-$(".navbar-toggle").click(
-    function () {
-        $('.navbar-menu').toggleClass("navbar-menu__active");
-    }
-);
+$(document).ready(function () {
+    $(".navbar-toggle").click(
+        function () {
+            $('.navbar-menu').toggleClass("navbar-menu__active");
+        }
+    );
 
-(function ($) {
-    $(function () {
+    (function ($) {
+        $(function () {
 
-        $('.blog-filter-select').styler();
+            $('.blog-filter-select').styler();
 
+        });
+    })(jQuery);
+
+    $('.related-posts-carousel').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 5,
+        nextArrow: '<span class="slick-next slick-arrow icon-next"></span>',
+        prevArrow: '<span class="slick-prev slick-arrow icon-prev"></span>',
+        responsive: [
+            {
+                breakpoint: 959,
+                settings: {
+                    slidesToShow: 3
+                }
+            }
+        ]
     });
-})(jQuery);
 
-$('.related-posts-carousel').slick({
-    dots: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 5,
-    nextArrow: '<span class="slick-next slick-arrow icon-next"></span>',
-    prevArrow: '<span class="slick-prev slick-arrow icon-prev"></span>',
-    responsive: [
-        {
-            breakpoint: 959,
-            settings: {
-                slidesToShow: 3
+    $('#myAffix').affix({
+        offset: {
+            top: 100,
+            bottom: function () {
+                return (this.bottom = $('.footer').outerHeight(true))
             }
         }
-    ]
+    })
+
 });
